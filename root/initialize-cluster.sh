98d789d64fbbb8645e69210811b0ef0e6b3a2467
Ross Gardler <ross@gardler.org>
2015-10-26 06:33:51 +0000

    change scripts to work with a smaller cluster

--- root/initialize-cluster.sh
+++ root/initialize-cluster.sh
@@ -6,7 +6,7 @@ container-service --cache-fingerprints
 container-service --list-agents
 container-service --list-masters
 container-service --start-agents
-container-service --scale-down 10
+container-service --scale-down 1
 container-service --docker-pull-agents azuredemo/web:azurecon --docker-pull-agents azuredemo/rest:azurecon
 container-service --delete-marathon-app azure
 wget https://raw.githubusercontent.com/rgardler/AzureDevTestDeploy/azurecon/marathon.json
